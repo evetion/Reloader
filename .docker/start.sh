54b2938f4624ad66b7e651b6205db295277ce4ac
Maarten Pronk <git@evetion.nl>
2025-04-24 22:03:37 +0200

    Try to fail the CI.

--- .docker/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-docker compose -f compose.yml up -d --force-recreate --remove-orphans
-echo "Installation of the plugin reloader"
-docker exec -t qgis sh -c "qgis_setup.sh reloader"
-echo "Containers are running"
